6199dff6031373f863a345ba3b2caea1dfc0d539
estigoldberger <113298758+estigoldberger@users.noreply.github.com>
2023-02-19 21:09:34 -0500

    Update 1 table.sql
    added planet to table

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -3,5 +3,6 @@ go
 drop table if exists body
 create table dbo.body (id int not null identity primary key,
 bodypart varchar(100) not null, 
-num int null
+num int null,
+planet varchar (50) not null
 )
